c224db2cc1c9901fe9eaeb90a8cffa38fb2398fe
silverio.di <silverio.di@qem.it>
2026-02-10 18:09:54 +0100

    added utils for qt

---
+++ python/examples/api_client_qt_demo/utils_qt.py
@@ -0,0 +1,35 @@
+"""Utilities for QT."""
+#-------------------------------------------------------------------------------
+# Name:         utils_qt
+#
+# Purpose:      Utilities for QT
+#
+# Note          Compatible with API server version 1.5.3
+#               1 (on 1.x.y) means interface contract
+#               x (on 1.x.y) means version
+#               y (on 1.x.y) means release
+#
+# Note          Checked with Python 3.11.9
+#
+# Author:       rosettacnc-classroom@gmail.com
+#
+# Created:      10/02/2026
+# Copyright:    RosettaCNC (c) 2016-2026
+# Licence:      RosettaCNC License 1.0 (RCNC-1.0)
+# Coding Style  https://www.python.org/dev/peps/pep-0008/
+#-------------------------------------------------------------------------------
+# pylint: disable=R0903 -> too-few-public-methods
+#-------------------------------------------------------------------------------
+from pathlib import Path
+from PySide6.QtWidgets import QApplication
+
+class QThemeManager:
+    """Xx..."""
+    def __init__(self, base_dir: str = 'styles'):
+        self.base_dir = Path(base_dir)
+
+    def load(self, theme_name: str) -> None:
+        """Xxx..."""
+        qss_path = self.base_dir / f'{theme_name}.qss'
+        qss = qss_path.read_text(encoding='utf-8')
+        QApplication.instance().setStyleSheet(qss)
