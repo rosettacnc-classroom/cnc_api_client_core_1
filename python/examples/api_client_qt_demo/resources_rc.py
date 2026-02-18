87c15b10956917c5a26f8d18d846e99586f18f32
silverio.di <silverio.di@qem.it>
2026-02-18 10:06:11 +0100

    added resource image and resources management

---
+++ python/examples/api_client_qt_demo/resources_rc.py
@@ -0,0 +1,75 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.10.2
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x02\x16\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2264\
+\x22 height=\x2264\x22 vi\
+ewBox=\x220 0 64 64\
+\x22>\x0d\x0a  <defs>\x0d\x0a  \
+  <radialGradien\
+t id=\x22g\x22 cx=\x2230%\
+\x22 cy=\x2230%\x22 r=\x2270\
+%\x22>\x0d\x0a      <stop\
+ offset=\x220%\x22 sto\
+p-color=\x22#ffffff\
+\x22 stop-opacity=\x22\
+0.95\x22/>\x0d\x0a      <\
+stop offset=\x2235%\
+\x22 stop-color=\x22{{\
+COLOR}}\x22 stop-op\
+acity=\x220.95\x22/>\x0d\x0a\
+      <stop offs\
+et=\x22100%\x22 stop-c\
+olor=\x22{{COLOR}}\x22\
+ stop-opacity=\x221\
+.0\x22/>\x0d\x0a    </rad\
+ialGradient>\x0d\x0a  \
+</defs>\x0d\x0a\x0d\x0a  <ci\
+rcle cx=\x2232\x22 cy=\
+\x2232\x22 r=\x2228\x22 fill\
+=\x22url(#g)\x22/>\x0d\x0a  \
+<circle cx=\x2232\x22 \
+cy=\x2232\x22 r=\x2228\x22 f\
+ill=\x22none\x22 strok\
+e=\x22#222\x22 stroke-\
+width=\x222\x22/>\x0d\x0a</s\
+vg>\x0d\x0a\
+"
+
+qt_resource_name = b"\
+\x00\x06\
+\x07\x03}\xc3\
+\x00i\
+\x00m\x00a\x00g\x00e\x00s\
+\x00\x10\
+\x07(\xd2g\
+\x00c\
+\x00i\x00r\x00c\x00u\x00l\x00a\x00r\x00-\x00l\x00e\x00d\x00.\x00s\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x03\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x12\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x9co\x82\xd5\xe3\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
